e813c461b977d6f40486640830c1b2fc84515a5c
Anushka7310 <61081130+Anushka7310@users.noreply.github.com>
2022-03-14 10:30:40 +0530

    added tabsets

--- renderPlot widget/ui.r
+++ renderPlot widget/ui.r
@@ -1,35 +1,56 @@
 
+
 library(shiny)
 shinyUI(fluidPage(
-
-
-  titlePanel(h4('Demostration of the renderPlot() - A Histogram with iris dataset', align = "center")),
+  titlePanel(
+    h4(
+      'Iris Dataset',
+      align = "center"
+    )
+  ),
 
 
   sidebarPanel(
+    selectInput(
+      "var",
+      label = "1. Select the quantitative Variable",
+      choices = c(
+        "Sepal.Length" = 1,
+        "Sepal.Width" = 2,
+        "Petal.Length" = 3,
+        "Petal.Width" = 4
+      ),
+      selected = 3
+    ),
 
 
+    sliderInput(
+      "bin",
+      "2. Select the number of histogram BINs by using the slider below",
+      min = 5,
+      max = 25,
+      value = 15
+    ),
 
-    selectInput("var", label = "1. Select the quantitative Variable",
-                choices = c("Sepal.Length" = 1, "Sepal.Width" = 2, "Petal.Length" = 3, "Petal.Width"=4),
-                selected = 3),
-
-
-    sliderInput("bin", "2. Select the number of histogram BINs by using the slider below", min=5, max=25, value=15),
-
-    radioButtons("colour", label = "3. Select the color of histogram",
-                 choices = c("Green", "Red",
-                             "Yellow"), selected = "Green")
+    radioButtons(
+      "colour",
+      label = "3. Select the color of histogram",
+      choices = c("Green", "Red",
+                  "Yellow"),
+      selected = "Green"
+    )
   ),
 
 
   mainPanel(
-    textOutput("text1"),
-    textOutput("text2"),
-    textOutput("text3"),
-    plotOutput("myhist")
+    tabsetPanel(
+      type = "tab",
+      tabPanel("Summary", verbatimTextOutput("sum")),
+      tabPanel("Structure", verbatimTextOutput("str")),
+      tabPanel("Data", tableOutput("data")),
+      tabPanel("Plot", plotOutput("myhist"))
+    )
 
   )
 
-)
-)
+))
